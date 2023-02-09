4b9fc90bd10214fbed9ec3b726f815384d2374e8
ajayi boluwatife <ajayiboluwatife85@gmail.com>
2023-02-09 18:09:41 +0100

    reactApp

---
+++ my-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="https://www.beinspirational.co.uk/awesome-from-our-friends-at-the-trinity-school/"><img src="https://www.beinspirational.co.uk/wp-content/uploads/2021/07/awesome-Rect-1024x781-1.jpg" className="App-logo" alt="PICTURE" width="200px" height="200px" /></a>
+        <p>
+          MY EMAIL INBOX
+        </p>
+        <a
+          //className="App-link"
+          href="https://mail.google.com/mail/u/1/#inbox"
+          //target="_blank"
+          //rel="noopener noreferrer"
+        >
+          just click
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
